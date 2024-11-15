15431712998b572801d2bdec8df53ceae0f84dc3
Alan Diniz Salazar <salazardiniz@yahoo.com.br>
2024-11-15 15:26:15 -0300

    Asset criado Menu: background, texto e som Const file criado

---
+++ code/Const.py
@@ -0,0 +1,16 @@
+# CORES
+C_WHITE = (255, 255, 255)
+C_BLACK = (0, 0, 0)
+C_ROAD_COLOR = (50, 50, 50)
+C_YELLOW = (255, 255, 0)
+C_ORANGE = (255, 128, 50)
+
+# MENU
+MENU_OPTION = ('SINGLE PLAYER',
+               'SCORE',
+               'EXIT')
+
+# TELA
+WIN_WIDTH = 512
+WIN_HEIGHT = 512
+
