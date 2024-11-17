19c90adba269af4d71cbec3ac34532e52182fda8
Alan Diniz Salazar <salazardiniz@yahoo.com.br>
2024-11-17 14:40:41 -0300

    Jogo finalizado - Versão 1.3 Implementado retorno ao Menu Imagens e sons ok Duas opções de carros Implementado damage nos carros Arquivos separados Score não implementado

--- code/Game.py
+++ code/Game.py
@@ -16,7 +16,7 @@ class Game:
             menu_return = menu.run()
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1]]:
-                level = Level(self.window, 'Level1', menu_return)
+                level = Level(self.window, 'Level', menu_return)
                 level_return = level.run()
 
                 # Se retornar 'MENU' após a colisão, recarrega o Menu
